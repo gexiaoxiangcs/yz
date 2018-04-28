8149660dd26e4c53662fdcffdee15ef5f46093ae
gexiaoxiang <295667158@qq.com>
2018-04-28 14:39:10 +0800

    init

--- api/common.class.php
+++ api/common.class.php
@@ -10,19 +10,9 @@
 class Common {
     public  function curl_get($url, $data = array(), $timeout = 3) {
         $curl = curl_init();
-        $headers = array(
-            "Content-type: application/json;charset='utf-8'",
-            "Accept: application/json",
-            "Cache-Control: no-cache",
-            "Pragma: no-cache",
-        );
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HEADER, $headers);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        if ($data) {
-            curl_setopt($curl, CURLOPT_POST, 1);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-        }
         $content = curl_exec($curl);
         curl_close($curl);
         return $content;
