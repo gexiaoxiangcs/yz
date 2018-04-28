9e61898250370bb6c97568409b57f82506e94d26
gexiaoxiang <295667158@qq.com>
2018-04-28 16:56:19 +0800

    init

--- api/login.class.php
+++ api/login.class.php
@@ -7,12 +7,42 @@
  * To change this template use File | Settings | File Templates.
  */
 include 'common.class.php';
+include 'memcache.class.php';
 class Login extends Common {
     public function getOpenid() {
         $code = $this->htmlencoder($_GET['code']);
         $appid = APPID;
         $appsecret = APPSECRET;
         $url = "https://api.weixin.qq.com/sns/jscode2session?appid=$appid&secret=$appsecret&js_code=$code&grant_type=authorization_code";
-         exit($this->curl_get($url));
+        $result = $this->curl_get($url);
+        $arr = json_decode($result,true);
+        if(!$arr['errcode']) {
+            $key = $this->trd_session(64);
+            $mem = new Memcache();
+            $mem->memcacheSet($result,$key,600);
+            $this->outPutJson(array(
+                'result' => 200,
+                'sessionid' => ,
+            ));
+        } else {
+
+        }
+
+        return false;
+    }
+
+    public function trd_session($len) {
+        $fp = @fopen('/dev/urandom','rb');
+        $result = '';
+        if ($fp !== FALSE) {
+            $result .= @fread($fp, $len);
+            @fclose($fp);
+        } else {
+            trigger_error('Can not open /dev/urandom.');
+            $result = time() . rand(1,100000000);
+        }
+        $result = base64_encode($result);
+        $result = strtr($result, '+/', '-_');
+        return substr($result, 0, $len);
     }
 }
---
+++ api/memcache.class.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Ge
+ * Date: 18-3-22
+ * Time: 下午4:21
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Mem{
+    public  function memcacheSet($data, $key, $time){
+        $this->memcacheInit();
+        $key = $this->_k($key);
+        return $this->memcon->set($key, $data, 0, $time);
+    }
+
+    public function memcacheGet($key){
+        $this->memcacheInit();
+        $key = $this->_k($key);
+        $result = $this->memcon->get($key);
+        return $result;
+    }
+
+    public function memcacheDel($key){
+        $this->memcacheInit();
+        $key = $this->_k($key);
+        return $this->memcon->delete($key);
+    }
+
+    public function memcacheIncr($key, $interval = 1){
+        $this->memcacheInit();
+        $key = $this->_k($key);
+        $result = $this->memcon->increment($key, $interval);
+        return $result;
+    }
+
+    public function memcacheDecr($key, $interval = 1){
+        $this->_memcacheInit();
+        $key = $this->_k($key);
+        $result = $this->memcon->decrement($key, $interval);
+        return $result;
+    }
+
+    private function memcacheInit(){
+        if(!$this->memcon){
+
+            $this->memcon = new Memcache();
+            $this->memcon->connect(MEM_HOST, MEM_PORT);
+        }
+    }
+
+    private function _k($k){
+        return md5($this->memprev . $k);
+    }
+}
